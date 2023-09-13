f7df397d29f390268665dd8b677a5ef95a2948a5
matthewhjt <matthewhjt72@gmail.com>
2023-09-13 13:08:07 +0700

    Added tests for response code, template, views context

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,22 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 
-# Create your tests here.
+class mainTest(TestCase):
+    def test_main_url_is_exist(self):
+        response = Client().get('/main/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_main_using_main_template(self):
+        response = Client().get('/main/')
+        self.assertTemplateUsed(response, 'main.html')
+
+    def test_main_model(self):
+        c = Client()
+        response = c.get('/main/')
+        context_test = {
+        'nama': 'Matthew Hotmaaja Johan Turnip',
+        'npm' : '2206081231',
+        'kelas': 'PBP C'
+        }
+        self.assertEqual(response.context['nama'], 'Matthew Hotmaraja Johan Turnip')
+        self.assertEqual(response.context['npm'], '2206081231')
+        self.assertEqual(response.context['kelas'], 'PBP C')
--- main/views.py
+++ main/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'nama': 'Matthew Hotmaaja Johan Turnip',
+        'nama': 'Matthew Hotmaraja Johan Turnip',
         'npm' : '2206081231',
         'kelas': 'PBP C'
     }
