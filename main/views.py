3085ff185fb0db4166ddfbefdb768cf9e18d5e54
matthewhjt <matthewhjt72@gmail.com>
2023-09-13 10:27:30 +0700

    Fixed main.html and main/views.py (now showing name, npm, class)

--- main/views.py
+++ main/views.py
@@ -3,10 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name': 'Leptup Geminh',
-        'amount': '10',
-        'price': '9000000',
-        'description': 'ini laptop katanya untuk kuliah tapi sebenernya buat geminh'
+        'nama': 'Matthew Hotmaaja Johan Turnip',
+        'npm' : '2206081231',
+        'kelas': 'PBP C'
     }
 
     return render(request, "main.html", context)
