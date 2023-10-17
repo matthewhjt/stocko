647d3ab2955e77d0fdacdbe5ab847252f91efe2b
matthewhjt <matthewhjt72@gmail.com>
2023-10-17 16:31:47 +0700

    More fixes regarding responsiveness

--- main/views.py
+++ main/views.py
@@ -80,6 +80,8 @@ def register(request):
             form.save()
             messages.success(request, 'Your account has been successfully created!')
             return redirect('main:login')
+        else:
+            messages.info(request, "Sorry, registration is invalid.")
     context = {'form':form}
     return render(request, 'register.html', context)
 
