1beadff9d377f03e42bf35bdb04e70b39054532e
matthewhjt <matthewhjt72@gmail.com>
2023-11-22 03:35:50 +0700

    Fixed STATIC_ROOT accidentally removed

--- stocko/settings.py
+++ stocko/settings.py
@@ -131,6 +131,7 @@ USE_TZ = True
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
